2542e6850476a23af89bd22858d689c0988ad088
Jimmy Ng <ngkwoklung@gmail.com>
2022-08-23 13:33:18 +0100

    Init commit

---
+++ src/main/java/com/sparta/jn/apitesting/App.java
@@ -0,0 +1,32 @@
+package com.sparta.jn.apitesting;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+            //HttpRequest, HttpClient, HttpResponse
+    {
+        HttpClient client = HttpClient.newHttpClient(); //can use var
+
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create("https://api.chucknorris.io/jokes/random"))
+                .build();
+
+        try {
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            System.out.println(response.statusCode());
+            System.out.println(response.body());
+
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
